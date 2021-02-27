9ecfa591342fc484230e5275d6d64adf84ca734d
Berlin Johnson <blackbear@Berlins-MacBook-Pro.local>
2021-02-27 11:19:23 +0900

    Add database mongo

--- Backend-Server/src/server.js
+++ Backend-Server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import { routes } from './routes';
+import { db } from './db';
 
 const app = express();
 const port = 8000;
@@ -12,6 +13,11 @@ routes.forEach((route) => {
   app[route.method](route.path, route.handler);
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+const start = async () => {
+  await db.connect('mongodb://localhost:27017');
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+  })
+}
+
+start();
